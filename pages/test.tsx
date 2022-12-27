055c7ace75a0fb4bae961e400f299915441f09dd
gufronnakaaw <gufronnakaaw@gmail.com>
2022-12-27 12:42:51 +0700

    update: test.tsx for get hostname

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,6 +1,22 @@
+import { GetServerSideProps } from 'next';
 
-export default function test() {
+type Params = {
+  hostname: string;
+};
+
+export default function test({ hostname }: Params) {
   return (
-    <div>deploy testing success</div>
-  )
+    <div>
+      <p>deploy testing success</p>
+      <p>your hostname is {hostname}</p>
+    </div>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  return {
+    props: {
+      hostname: req.headers.host,
+    },
+  };
+};
